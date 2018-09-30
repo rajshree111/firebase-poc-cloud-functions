c96737179ef1f217b0c6ecd6244bf0748d1170f6
test <test@CORP.AMAZONWORKSPACES.COM>
2018-09-30 11:39:01 +0530

    'initial'

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require('firebase-functions');
+const cors = require("cors")({ origin: true });
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+var firebaseClient = require('firebase');
+
+exports.testFunc = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {});
+  res.set('Access-Control-Allow-Origin', "*");
+  res.set('Access-Control-Allow-Methods', 'GET, POST');
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://test-auth-a.firebaseio.com"
+  })
+  
+
+  console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + req.body.email);
+
+  admin.auth().getUserByEmail(req.body.email)
+  .then(function(userRecord) {
+    var uid = userRecord.uid;
+    console.log('####################################' + theToken(uid));
+    return theToken(uid,res);
+  })
+  .catch(function(error) {
+    console.log("Error fetching user data:", error);
+  });
+})
+
+theToken = function(uid,res){
+  admin.auth().createCustomToken(uid)
+    .then(function(customToken) {
+            return res.status(200).json({ 
+                  message: customToken.toString()
+      
+                });
+    })
+    .catch(function(error) {
+      console.log("Error creating custom token:", error);
+    });
+}
